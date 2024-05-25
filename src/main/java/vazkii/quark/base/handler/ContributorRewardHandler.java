9643a7b9a978c79507609b22d7cd818b97720d67
DGun Otto <id_cn00@outlook.com>
2024-05-25 22:21:32 +0800

    Fuck u Quark

--- src/main/java/vazkii/quark/base/handler/ContributorRewardHandler.java
+++ src/main/java/vazkii/quark/base/handler/ContributorRewardHandler.java
@@ -124,20 +124,6 @@ public class ContributorRewardHandler {
 
 		@Override
 		public void run() {
-			try {
-				URL url = new URL("https://raw.githubusercontent.com/Vazkii/Quark/master/contributors.properties");
-				URLConnection conn = url.openConnection();
-				conn.setConnectTimeout(10*1000);
-				conn.setReadTimeout(10*1000);
-				
-				Properties patreonTiers = new Properties();
-				try (InputStreamReader reader = new InputStreamReader(conn.getInputStream())) {
-					patreonTiers.load(reader);
-					load(patreonTiers);
-				}
-			} catch (IOException e) {
-				Quark.LOG.error("Failed to load patreon information", e);
-			}
 		}
 
 	}
